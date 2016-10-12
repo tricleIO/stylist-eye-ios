60be2ddd3d33ae7824c51dd9e679bb37b3fbca0f
Michal Severín <michalseverin@Michal-MacBook-Pro.local>
2016-10-12 12:50:38 +0200

    Added architecture

---
+++ StylistEye/Essentials/Controllers/Abstract/AbstractViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AbstractViewController.swift
+//  StylistEye
+//
+//  Created by Michal Severín on 12.10.16.
+//  Copyright © 2016 Michal Severín. All rights reserved.
+//
+
+import UIKit
+
+/**
+ Abstract view controller setup initializable protocol to life-cycle methods.
+ */
+class AbstractViewController: UIViewController, InitializableProtocol {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        /// Load data from API every time.
+        loadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /// Just added elements to view and apply constraints.
+        addElementsAndApplyConstraints()
+    }
+
+    // MARK: - <Initializable>
+    func addElements() {}
+    func customInit() {}
+    func initializeElements() {}
+    func setupConstraints() {}
+    func loadData() {}
+    func setupView() {}
+}
---
+++ StylistEye/Services/NetworkExecutable.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkExecutable.swift
+//  StylistEye
+//
+//  Created by Michal Severín on 12.10.16.
+//  Copyright © 2016 Michal Severín. All rights reserved.
+//
+
+import Foundation
